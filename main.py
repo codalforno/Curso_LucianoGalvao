82076d56ea420a382fb6d826a50b97b9dcc691da
Christianne Dalforno <profcod@gmail.com>
2024-06-06 08:29:54 -0300

    meu primeiro commit

---
+++ main.py
@@ -0,0 +1,18 @@
+print("Jornada de dados")
+
+idade=48
+nome="Christianne"
+
+print(idade)
+
+nova_idade=idade+1
+print(nova_idade)
+
+lista=[1, 2, 3, 4, 5]
+print(lista)
+lista_invertida = lista.reverse()
+print(lista_invertida)
+print(lista.reverse())
+print(lista)
+lista.reverse()
+print(lista)
